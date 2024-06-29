6d6324f064e11250d954f3c09b60bb2eba0f8820
ishaqshaikhh <ishaqshaikh401@gmail.com>
2024-06-29 18:20:36 +0530

    ishaq 6

--- backend/mainapp/urls.py
+++ backend/mainapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("api/getAllProducts/", views.getAllProducts, name="getAllProducts"),
@@ -10,4 +12,4 @@ urlpatterns = [
     path("api/removeCart/", views.remove_cart, name="removeCart"),
     path("",include("accounts.urls"))
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

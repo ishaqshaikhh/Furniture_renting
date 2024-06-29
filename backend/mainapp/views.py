0f461327a11389c238db181dd0fcb1eea14d2de1
ishaqshaikhh <ishaqshaikh401@gmail.com>
2024-06-29 12:35:38 +0530

    ishaq 12:35

--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+def handle_404():
+    return JsonResponse({'status': 404, 'message': '404 Not Found'})

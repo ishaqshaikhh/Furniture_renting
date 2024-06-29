b1be9f8ba1aa6afd3db97334d043fbe5f72df9e8
aatif shaikh <aatifshaikh4564@gmail.com>
2024-06-29 13:29:03 +0530

    Aatif

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,6 +3,7 @@ import styles from '../styles/navbar.module.css'
 import { Link } from 'react-router-dom'
 import { FiSearch, FiHeart } from "react-icons/fi";
 import { BiShoppingBag, BiUser } from "react-icons/bi";
+import { RiMenu2Line } from "react-icons/ri";
 
 
 const Navbar = () => {
@@ -10,10 +11,11 @@ const Navbar = () => {
         <>
             <nav className={styles.navbar}>
                 <ul className={styles.menu_links}>
-                    <li><Link>Home</Link></li>
-                    <li><Link>Products</Link></li>
-                    <li><Link>Contact Us</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                 </ul>
+                <RiMenu2Line style={{cursor: "pointer"}} className={styles.menu_icon} />
                 <div className={styles.logo}>
                     <img src="/images/logo.png" className='img-fluid' alt="" />
                 </div>
